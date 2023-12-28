9087f9f59093ab3aba532ac5c56fa9b41fb6d8a2
Igor Becker <igor_nyu@hotmail.com>
2023-12-28 08:15:39 -0300

    feat: dequeue

--- src/lib/dequeue.ts
+++ src/lib/dequeue.ts
@@ -1,58 +1,104 @@
 export class Dequeue<T> {
-  private items: {[key: number]: T};
-  private count: number;
-  private lowCount: number;
+    private items: { [key: number]: T };
+    private count: number;
+    private lowCount: number;
 
-  constructor() {
-    this.items = {};
-    this.count = 0;
-    this.lowCount = 0;
-  }
-
-  public addFront(element: T): void {}
-
-  public addBack(element: T): void {}
-
-  public peekFront(): T {}
-
-  public peekBack(): T {}
-
-  public removeFront(): T {}
-
-  public removeBack(): T {}
-
-  public isEmpty(): boolean {
-    return this.count === this.lowCount;
-  }
-
-  public size(): number {
-    return this.count - this.lowCount;
-  }
-
-  public clear(): void {
-    this.items = {};
-    this.count = 0;
-    this.lowCount = 0;
-  }
-
-  public clone(): Dequeue<T> {
-    const dequeue = new Dequeue<T>();
-
-    for (let i = 0; i <= this.size() - 1; i++) {
-      dequeue.addFront(this.items[i]);
+    constructor() {
+        this.items = {};
+        this.count = 0;
+        this.lowCount = 0;
     }
 
-    return dequeue;
-  }
+    public addFront(element: T): void {
+        if (this.isEmpty()) {
+            this.addBack(element);
+        } else if (this.lowCount > 0) {
+            this.lowCount--;
+            this.items[this.lowCount] = element;
+        } else {
+            const newItems = {} as { [key: number]: T };
+            newItems[0] = element;
 
-  public to_string(): string {
-    const cloned = this.clone();
-    let result = '';
+            for (let i = 1; i <= this.count + 1; i++) {
+                newItems[i] = this.items[i - 1];
+            }
 
-    for (let i = 0; (i = cloned.size()); i++) {
-      result += `${cloned.removeBack()}`;
+            this.items = newItems;
+        }
+
+        this.count++;
+        this.lowCount = 0;
     }
 
-    return result;
-  }
+    public addBack(element: T): void {
+        this.items[this.count] = element;
+        this.count++;
+    }
+
+    public peekFront(): T {
+        return this.items[this.lowCount];
+    }
+
+    public peekBack(): T {
+        return this.items[this.count];
+    }
+
+    public removeFront(): T | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+
+        const value = this.items[this.lowCount];
+        delete this.items[this.lowCount];
+
+        this.lowCount++;
+        return value;
+    }
+
+    public removeBack(): T | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+
+        const value = this.items[this.count];
+        delete this.items[this.count];
+
+        this.count--;
+        return value;
+    }
+
+    public isEmpty(): boolean {
+        return this.count === this.lowCount;
+    }
+
+    public size(): number {
+        return this.count - this.lowCount;
+    }
+
+    public clear(): void {
+        this.items = {};
+        this.count = 0;
+        this.lowCount = 0;
+    }
+
+    public clone(): Dequeue<T> {
+        const dequeue = new Dequeue<T>();
+
+        for (let i = 0; i <= this.size() - 1; i++) {
+            dequeue.addFront(this.items[i]);
+        }
+
+        return dequeue;
+    }
+
+    public to_string(): string {
+        const cloned = this.clone();
+        let result = "";
+
+        for (let i = 0; (i = cloned.size()); i++) {
+            result += `${cloned.removeBack()}`;
+        }
+
+        return result;
+    }
 }
