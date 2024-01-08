e3f83232c541fda664dacb9c0313956d181eb3fe
Igor Becker <igor_nyu@hotmail.com>
2024-01-08 16:03:31 -0300

    feat: init dequeue

--- src/lib/dequeue.ts
+++ src/lib/dequeue.ts
@@ -10,94 +10,35 @@ export class Dequeue<T> {
     }
 
     public addFront(element: T): void {
-        if (this.isEmpty()) {
-            this.addBack(element);
-        } else if (this.lowCount > 0) {
-            this.lowCount--;
-            this.items[this.lowCount] = element;
-        } else {
-            const newItems = {} as { [key: number]: T };
-            newItems[0] = element;
-
-            for (let i = 1; i <= this.count + 1; i++) {
-                newItems[i] = this.items[i - 1];
-            }
-
-            this.items = newItems;
-        }
-
-        this.count++;
     }
 
     public addBack(element: T): void {
-        this.items[this.count] = element
-        this.count++ 
     }
 
     public peekFront(): T {
-        return this.items[this.lowCount];
     }
 
     public peekBack(): T {
-        return this.items[this.count - 1];
     }
 
     public removeFront(): T | undefined {
-        if (this.isEmpty()) {
-            return undefined;
-        }
-
-        const value = this.items[this.lowCount];
-        delete this.items[this.lowCount];
-
-        this.lowCount++;
-        return value;
     }
 
     public removeBack(): T | undefined {
-        if (this.isEmpty()) {
-            return undefined;
-        }
-
-        const value = this.items[this.count];
-        delete this.items[this.count];
-
-        this.count--;
-        return value;
     }
 
     public isEmpty(): boolean {
-        return this.count === this.lowCount;
     }
 
     public size(): number {
-        return this.count - this.lowCount;
     }
 
     public clear(): void {
-        this.items = {};
-        this.count = 0;
-        this.lowCount = 0;
     }
 
     public clone(): Dequeue<T> {
-        const dequeue = new Dequeue<T>();
-
-        for (let i = 0; i <= this.size() - 1; i++) {
-            dequeue.addBack(this.items[i]);
-        }
-
-        return dequeue;
     }
 
     public to_string(): string {
-        const cloned = this.clone();
-        let result = "";
-
-        for (let i = 0; (i <= cloned.size() - 1); i++) {
-            result += `${cloned.removeBack()}`;
-        }
-
-        return result;
     }
 }
--- src/tests/lib/dequeue.test.ts
+++ src/tests/lib/dequeue.test.ts
@@ -1,98 +1,97 @@
-import {expect, it, describe} from 'bun:test';
+import { expect, it, describe } from "bun:test";
 
-import {Dequeue} from '@lib/dequeue';
+import { Dequeue } from "@lib/dequeue";
 
-describe('Dequeue suite', () => {
+describe("Dequeue suite", () => {
     it("should create an empty Dequeue", () => {
-        const dequeue = new Dequeue<number>()
+        const dequeue = new Dequeue<number>();
 
-        expect(dequeue.isEmpty()).toBeTrue()
-    })
+        expect(dequeue.isEmpty()).toBeTrue();
+    });
 
     it("should add new items at the back to the queue", () => {
-        const dequeue = new Dequeue<number>()
+        const dequeue = new Dequeue<number>();
 
-        dequeue.addBack(1)
-        dequeue.addBack(2)
+        dequeue.addBack(1);
+        dequeue.addBack(2);
 
-        expect(dequeue.isEmpty()).toBeFalse()
-        expect(dequeue.size()).toBe(2)
-        expect(dequeue.peekBack()).toBe(2)
-        expect(dequeue.peekFront()).toBe(1)
-    })
+        expect(dequeue.isEmpty()).toBeFalse();
+        expect(dequeue.size()).toBe(2);
+        expect(dequeue.peekBack()).toBe(2);
+        expect(dequeue.peekFront()).toBe(1);
+    });
 
     it("should add new items at the front to the queue", () => {
-        const dequeue = new Dequeue<number>()
+        const dequeue = new Dequeue<number>();
 
-        dequeue.addFront(1)
-        dequeue.addFront(2)
+        dequeue.addFront(1);
+        dequeue.addFront(2);
 
-        expect(dequeue.isEmpty()).toBeFalse()
-        expect(dequeue.size()).toBe(2)
-        expect(dequeue.peekFront()).toBe(2)
-        expect(dequeue.peekBack()).toBe(1)
-    })
+        expect(dequeue.isEmpty()).toBeFalse();
+        expect(dequeue.size()).toBe(2);
+        expect(dequeue.peekFront()).toBe(2);
+        expect(dequeue.peekBack()).toBe(1);
+    });
 
     it("should remove the first item from the Dequeue", () => {
-        const dequeue = new Dequeue<number>()
+        const dequeue = new Dequeue<number>();
 
-        dequeue.addBack(1)
-        dequeue.addBack(2)
+        dequeue.addBack(1);
+        dequeue.addBack(2);
 
-        expect(dequeue.size()).toBe(2)
+        expect(dequeue.size()).toBe(2);
 
-        dequeue.removeFront()
+        dequeue.removeFront();
 
-        expect(dequeue.size()).toBe(1)
-        expect(dequeue.peekFront()).toBe(2)
-    })
+        expect(dequeue.size()).toBe(1);
+        expect(dequeue.peekFront()).toBe(2);
+    });
 
     it("should remove the last item from the Dequeue", () => {
-        const dequeue = new Dequeue<number>()
+        const dequeue = new Dequeue<number>();
 
-        dequeue.addBack(1)
-        dequeue.addBack(2)
+        dequeue.addBack(1);
+        dequeue.addBack(2);
 
-        expect(dequeue.size()).toBe(2)
+        expect(dequeue.size()).toBe(2);
 
-        dequeue.removeBack()
+        dequeue.removeBack();
 
-        expect(dequeue.size()).toBe(1)
-        expect(dequeue.peekBack()).toBe(1)
-    })
+        expect(dequeue.size()).toBe(1);
+        expect(dequeue.peekBack()).toBe(1);
+    });
 
     it("should clear the Dequeue", () => {
-        const dequeue = new Dequeue<number>()
+        const dequeue = new Dequeue<number>();
 
-        dequeue.addBack(1)
-        dequeue.addBack(2)
+        dequeue.addBack(1);
+        dequeue.addBack(2);
 
-        expect(dequeue.size()).toBe(2)
+        expect(dequeue.size()).toBe(2);
 
-        dequeue.clear()
+        dequeue.clear();
 
-        expect(dequeue.isEmpty()).toBeTrue()
-    })
+        expect(dequeue.isEmpty()).toBeTrue();
+    });
 
+    it("should be possible to create a clone of the Dequeue", () => {
+        const dequeue = new Dequeue<number>();
 
-    it ("should be possible to create a clone of the Dequeue", () => {
-        const dequeue = new Dequeue<number>()
+        dequeue.addBack(1);
+        dequeue.addBack(2);
 
-        dequeue.addBack(1)
-        dequeue.addBack(2)
+        const clone = dequeue.clone();
 
-        const clone = dequeue.clone()
-
-        expect(clone.size()).toBe(2)
-        expect(clone.peekFront()).toBe(1)
-    })
+        expect(clone.size()).toBe(2);
+        expect(clone.peekFront()).toBe(1);
+    });
 
     it("should return the Dequeue stringifyed", () => {
-        const dequeue = new Dequeue<number>()
+        const dequeue = new Dequeue<number>();
 
-        dequeue.addBack(1)
-        dequeue.addBack(2)
+        dequeue.addBack(1);
+        dequeue.addBack(2);
 
-        expect(dequeue.to_string()).toBe("12")
-    })
+        expect(dequeue.to_string()).toBe("12");
+    });
 });
